21e814bcb76c66cc1d3a154c16d81fb5f9469890
kraterkraken <47699838+kraterkraken@users.noreply.github.com>
2020-04-04 09:50:00 -0400

    moved usage comment to the readme file

--- csvpresto.py
+++ csvpresto.py
@@ -1,10 +1,3 @@
-#
-# usage: csvpresto -g <collist> -s <collist> -o <SUM|COUNT|AVG> -f filename
-# where -g specifies the list of columns to group by.  Ex: 1 2 3 4
-#       -s specifies the columns to gather stats on.  Ex: 5 6 7
-#       -o specifies the statistical operation to perform (SUM | COUNT | AVG)
-#       -f specifies the CSV file to use as input
-#
 from argparse import ArgumentParser
 import sys
 
